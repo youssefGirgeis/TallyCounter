8bbb685e8cff29cfda88d96940d9559df8d586b0
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-09-20 23:36:10 -0400

    add click listener

--- app/src/main/java/com/example/tallycounter/MainActivity.java
+++ app/src/main/java/com/example/tallycounter/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.tallycounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int tally;
+    private TextView tallyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tally = 0;
+
+        Button tallyButton = (Button)findViewById(R.id.TallyButton);
+        tallyButton.setOnClickListener(onClickTallyButton);
+
+        tallyTextView = (TextView)findViewById(R.id.TallyText);
+
     }
+
+    private View.OnClickListener onClickTallyButton = new View.OnClickListener(){
+
+
+        @Override
+        public void onClick(View view) {
+            tally++;
+            //System.out.println(tally);
+            tallyTextView.setText(String.valueOf(tally));
+        }
+    };
 }
